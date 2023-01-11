ed519b5b5dd37f97e8b42abf954729002ff9ea8f
Leonardo Miranda <leonardmiranda99@gmail.com>
2023-01-11 00:44:51 -0600

    mantenimiento, correcion de errores, proyectos abre en una pagina diferente

--- hola nombre.js
+++ hola nombre.js
@@ -1,4 +1,4 @@
 function changeHelloName(){
     let name = prompt ("Escribe tu nombre");
-    document.getElementById("saludo").innerHTML ="Hola " + name;
+    document.getElementById("saludo").innerHTML ="Hola, " + name;
 }
